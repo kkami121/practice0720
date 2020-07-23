0466efa16956956ff792342e0b9935340c94d300
kkami121 <kkami121@naver.com>
2020-07-23 16:43:53 +0900

    Create 0720_boolean.java
    ==연산자는 같은 객체를 비교하므로 같은 단어여도 객체가 다르면 false
    
    .equals는 다른 객체여도 같으면 ture

---
+++ 0720_boolean.java
@@ -0,0 +1,9 @@
+package practice0720;
+
+class EqualStringDemo{
+    public static void main(String[] args){
+        String a = "Hello";
+        String b = "Hello";
+        System.out.println(a == b);
+    }
+}
